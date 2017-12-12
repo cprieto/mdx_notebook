6d9122cbeb0491c4a0b6f73a1b56a879352e7679
Cristian Prieto <me@cprieto.com>
2017-12-12 17:11:06 +0100

    Add configuration and factory
     * The configuation element for output style
     * The meta command to create the extension

--- mdx_notebook/__init__.py
+++ mdx_notebook/__init__.py
@@ -2,20 +2,34 @@ from markdown.extensions import Extension
 from markdown.inlinepatterns import Pattern
 from markdown.util import etree
 
-OUTPUT_RE = r'(\|\[\]>)(.*?)\|\[\]>'
+OUTPUT_RE = r'(\|\[\]>)(.*?)<\[\]\|'
 
 
 class NotebookPattern(Pattern):
+    def __init__(self, pattern, output_class='notebook_output'):
+        self.output_class = output_class
+        super(NotebookPattern, self).__init__(pattern)
+
     def handleMatch(self, m):
         el = etree.Element('pre')
-        el.set('class', 'notebook_output')
+        el.set('class', self.output_class)
         el.text = m.group(3)
 
         return el
 
 
 class Notebook(Extension):
+    def __init__(self, *args, **kwargs):
+        self.config = {
+            'output_class': ['notebook_output', 'Class to apply to the output pre block']
+        }
+        super(Notebook, self).__init__(*args, **kwargs)
+
     def extendMarkdown(self, md, md_globals):
-        notebook_pattern = NotebookPattern(OUTPUT_RE)
+        notebook_pattern = NotebookPattern(OUTPUT_RE,
+                                           output_class=self.getConfig('output_class'))
         md.inlinePatterns['notebook_output'] = notebook_pattern
 
+
+def makeExtension(*args, **kwargs):
+    return Notebook(*args, **kwargs)
